ca54be8300140ee22c70a0b493c5d1643fc26192
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-02-16 16:36:40 -0800

    simple app with react

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'jsx');
+
+var options = {
+  jsx: {
+    harmony: true,
+    beautify: true
+  }
+};
+
+app.engine('jsx', require('express-react-views').createEngine(options));
+
+app.get('/', require('./routes').index);
+
+var server = app.listen(3000, function () {
+  var host = server.address().address,
+      port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
