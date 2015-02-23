f86c657b39e905640b4bd20339b2685723aacc20
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-02-23 01:36:49 -0800

    add watch task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,14 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.config('watch', {
+    react: {
+      files: 'views/**/*.jsx',
+      tasks: ['browserify']
+    }
+  });
+
   grunt.config('browserify', {
     options: {
       transform:  [ require('grunt-react').browserify ]
