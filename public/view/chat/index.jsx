b623328bfeb84b6b0340b043ab6e40ee918eb843
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-02-24 00:42:04 -0800

    add missing client entry-point

---
+++ public/view/chat/index.jsx
@@ -0,0 +1,14 @@
+var React = require('react'),
+    $ = require('jquery'),
+    CommentBox = require('../../../views/chat/CommentBox.jsx');
+
+$(function() {
+    var containerEl = document.getElementById('commentBox'),
+	data = JSON.parse(document.getElementById('initial-data').innerHTML);
+    data[0].author = 'client 1';
+    console.log(data);
+    React.render(
+	<CommentBox data={data} />,
+	containerEl
+    );
+});
